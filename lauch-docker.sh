8a1f21956b54d967b3fa4f438f8414fa0f4cc6a0
Matias Dahl <matias.dahl@iki.fi>
2018-07-21 13:26:32 +0300

    Create lauch-docker.sh

---
+++ lauch-docker.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# https://hub.docker.com/r/matiasdahl/backend/
+sudo docker pull matiasdahl/backend:latest
+
+export PORT_INSIDE_DOCKER=8080
+export PORT_EXPOSED=80
+export DATA_DIR=`pwd`/backend/src/test/resources/testdata/3-aggregates/
+#export DATA_DIR=`pwd`/../agg-data/
+
+sudo docker run --rm -it \
+  -p $PORT_EXPOSED:$PORT_INSIDE_DOCKER \
+  -v $DATA_DIR:/data/ \
+  -e DATA_DIRECTORY=/data/ \
+  -e OSM_SOURCE_DOWNLOADED="2018.5.31" \
+  -e OSM_SOURCE_MD5="1234fffffaabbccdd11aaa1111111123" \
+  -e HTTP_PORT=$PORT_INSIDE_DOCKER \
+  -e HTTP_INTERFACE_NAME=0.0.0.0 \
+  -e SELECTED_TAGS=amenity,barrier,building,highway,landuse,leisure,man_made,natural,railway,shop,sport,surface,tourism \
+  matiasdahl/backend:latest
